65151a073a9a6c3babaff2e54f6e23759fae84da
vertexnormal <vertexnormal@gmail.com>
2013-08-02 16:57:45 -0600

    Further work on integrating polyvox.

--- Volume/marchvolume.h
+++ Volume/marchvolume.h
@@ -9,9 +9,6 @@
 
 namespace anl
 {
-	int marchCube(CArray3Dd &df, unsigned int x, unsigned int y, unsigned int z, float isolevel, anl::CVec3f triangles[15]);
-	int marchTetra(CArray3Dd &df,unsigned int x, unsigned int y, unsigned int z, double iso,anl::CVec3f tri[6],int v0,int v1,int v2,int v3);
-	void marchDensityFieldSurfaceOBJ(CArray3Dd &df, float iso, std::string name);
 	void marchDensityFieldOBJ(CArray3Dd &df, float iso, std::string name);
 };
 
