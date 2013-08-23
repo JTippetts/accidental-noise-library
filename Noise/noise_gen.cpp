8151bac07c7395f48b3690e7275e66258d40e357
vertexnormal <vertexnormal@gmail.com>
2013-08-23 13:30:27 -0600

    Move FNV hash away from pointer/array based solution in favor of a combiner-type solution, to keep from having to fiddle with pointers.

--- Noise/noise_gen.cpp
+++ Noise/noise_gen.cpp
@@ -220,6 +220,13 @@ unsigned int fnv_32_a_buf(void *buf, unsigned int len)
     return hval;
 }
 
+unsigned int fnv_32_a_combine(unsigned int hash, unsigned int val)
+{
+	hash^=val;
+	hash*=FNV_32_PRIME;
+	return hash;
+}
+
 
 unsigned char xor_fold_hash(unsigned int hash)
 {
@@ -231,25 +238,52 @@ unsigned char xor_fold_hash(unsigned int hash)
 // FNV-based coordinate hashes
 unsigned int hash_coords_2(int x, int y, unsigned int seed)
 {
-    unsigned int d[3]={(unsigned int)x,(unsigned int)y,seed};
-    return xor_fold_hash(fnv_32_a_buf(d, 3));
+    //unsigned int d[3]={(unsigned int)x,(unsigned int)y,seed};
+    //return xor_fold_hash(fnv_32_a_buf(d, 3));
+	unsigned int hash=FNV_32_INIT;
+	hash=fnv_32_a_combine(hash,(unsigned int)x);
+	hash=fnv_32_a_combine(hash,(unsigned int)y);
+	hash=fnv_32_a_combine(hash,seed);
+	return xor_fold_hash(hash);
 }
 unsigned int hash_coords_3(int x, int y, int z, unsigned int seed)
 {
-    unsigned int d[4]={(unsigned int)x,(unsigned int)y,(unsigned int)z,seed};
-    return xor_fold_hash(fnv_32_a_buf(d, 4));
+    //unsigned int d[4]={(unsigned int)x,(unsigned int)y,(unsigned int)z,seed};
+    //return xor_fold_hash(fnv_32_a_buf(d, 4));
+	unsigned int hash=FNV_32_INIT;
+	hash=fnv_32_a_combine(hash,(unsigned int)x);
+	hash=fnv_32_a_combine(hash,(unsigned int)y);
+	hash=fnv_32_a_combine(hash,(unsigned int)z);
+	hash=fnv_32_a_combine(hash,seed);
+	return xor_fold_hash(hash);
 }
 
 unsigned int hash_coords_4(int x, int y, int z, int w, unsigned int seed)
 {
-    unsigned int d[5]={(unsigned int)x,(unsigned int)y,(unsigned int)z,(unsigned int)w,seed};
-    return xor_fold_hash(fnv_32_a_buf(d, 5));
+    //unsigned int d[5]={(unsigned int)x,(unsigned int)y,(unsigned int)z,(unsigned int)w,seed};
+    //return xor_fold_hash(fnv_32_a_buf(d, 5));
+	unsigned int hash=FNV_32_INIT;
+	hash=fnv_32_a_combine(hash,(unsigned int)x);
+	hash=fnv_32_a_combine(hash,(unsigned int)y);
+	hash=fnv_32_a_combine(hash,(unsigned int)z);
+	hash=fnv_32_a_combine(hash,(unsigned int)w);
+	hash=fnv_32_a_combine(hash,seed);
+	return xor_fold_hash(hash);
 }
 
 unsigned int hash_coords_6(int x, int y, int z, int w, int u, int v, unsigned int seed)
 {
-    unsigned int d[7]={(unsigned int)x,(unsigned int)y,(unsigned int)z,(unsigned int)w,(unsigned int)u,(unsigned int)v,seed};
-    return xor_fold_hash(fnv_32_a_buf(d, 7));
+    //unsigned int d[7]={(unsigned int)x,(unsigned int)y,(unsigned int)z,(unsigned int)w,(unsigned int)u,(unsigned int)v,seed};
+    //return xor_fold_hash(fnv_32_a_buf(d, 7));
+	unsigned int hash=FNV_32_INIT;
+	hash=fnv_32_a_combine(hash,(unsigned int)x);
+	hash=fnv_32_a_combine(hash,(unsigned int)y);
+	hash=fnv_32_a_combine(hash,(unsigned int)z);
+	hash=fnv_32_a_combine(hash,(unsigned int)w);
+	hash=fnv_32_a_combine(hash,(unsigned int)u);
+	hash=fnv_32_a_combine(hash,(unsigned int)v);
+	hash=fnv_32_a_combine(hash,seed);
+	return xor_fold_hash(hash);
 }
 
 unsigned int compute_hash_ANLFloatType_2(ANLFloatType x, ANLFloatType y, unsigned int seed)
