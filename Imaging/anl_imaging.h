860074b9bd65f4f212cc6ab1e8b0f328a51bfd14
vertexnormal <vertexnormal@gmail.com>
2013-09-18 15:47:49 -0600

    Added CRGBABufferNormalMap for generating normal maps from an implicit buffer.

--- Imaging/anl_imaging.h
+++ Imaging/anl_imaging.h
@@ -25,6 +25,7 @@
 #include "rgbabufferimplicitbufferadapter.h"
 #include "rgbabufferblur.h"
 #include "rgbabufferimplicitbuffermultiply.h"
+#include "rgbabuffernormalmap.h"
 #include "rgbabufferselect.h"
 
 #endif
